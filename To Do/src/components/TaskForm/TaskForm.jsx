8e63be60468ece9aa901f28eb69855998e6f0703
Rahul Sah <rahulmsd0777@gmail.com>
2023-08-09 20:47:19 +0530

    Created the TextForm component

--- To Do/src/components/TaskForm/TaskForm.jsx
+++ To Do/src/components/TaskForm/TaskForm.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from 'react';
+import './TaskForm.css';
+
 
 export const TaskForm = () => {
   return (
-    <div>TaskForm</div>
+    <>
+    <div className='container'>        
+        <h1 className='heading'>Add a new task</h1>
+        <label>Enter the task:</label>
+        <input className='input__task' placeholder='Task'></input>
+        <label>Select priority:</label>  
+        <select className='input__priority'>
+            <option className='priority--high'>High</option>
+            <option className='priority--medium'>Medium</option> 
+            <option className='priority--low'>Low</option>
+        </select>
+    </div>
+    
+</>
   )
 }
