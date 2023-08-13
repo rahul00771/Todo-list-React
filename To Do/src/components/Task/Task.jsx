3db05478a44b1191936618cf748e999ad2c87b87
Rahul Sah <rahulmsd0777@gmail.com>
2023-08-14 00:53:18 +0530

    Replaced buttons with mui buttons

--- To Do/src/components/Task/Task.jsx
+++ To Do/src/components/Task/Task.jsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import './Task.css'
+
+import React from "react";
+import './Task.css';
+import Stack from '@mui/material/Stack';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import Button from '@mui/material/Button';
+
 
 export const Task = (props) => {
     const {id, task, priority, isCompleted} = props;
@@ -8,8 +15,17 @@ export const Task = (props) => {
         <article className='card'>
             <h2 className='card__task'>Task : {task}</h2>
             <h4 className='card__priority'>Priority : {priority}</h4>
-            <button className='card__btn'>edit</button>            
-            <button className='card__btn'>delete</button>            
+            {/* <button className='card__btn'>edit</button>            
+            <button className='card__btn'>delete</button>                   */}
+        
+            <Stack direction="row-reverse" spacing={1}>
+                <IconButton color="error">
+                    <DeleteIcon/>
+                </IconButton>
+                <Button variant="outlined" size="small" endIcon={<EditIcon/>}>Edit</Button>
+                
+            </Stack>    
+            
         </article>
     </>
   )
--- To Do/src/components/TaskForm/TaskForm.jsx
+++ To Do/src/components/TaskForm/TaskForm.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import './TaskForm.css';
 import {Link} from 'react-router-dom';
+import { Button } from '@mui/material';
+import { Stack } from '@mui/material';
+import {Input} from '@mui/base/Input';
 
 
 
@@ -10,19 +13,25 @@ export const TaskForm = () => {
     <div className='container'>        
         <h1 className='heading'>Add a new task</h1>
         <label>Enter the task:</label>
-        <input className='input__task' placeholder='Task'></input>
+        <input className='input__task' placeholder='Task'></input>        
         <label>Select priority:</label>  
         <select className='input__priority'>
             <option className='priority--high'>High</option>
             <option className='priority--medium'>Medium</option> 
             <option className='priority--low'>Low</option>
-        </select>
-        <section className="btn-container">
+        </select>        
+
+        <section className='btn-container'>
+          <Stack direction='row' spacing={4}>
           <Link to='/'>
-            <button className='btn'>Cancel</button>
+            <Button variant='contained'>Cancel</Button>
           </Link>
-          <button className='btn' onClick={()=>console.log("Added task")}>Save</button>
+            <Button variant='outlined'>Save</Button>
+          </Stack>
         </section>
+
+        
+
     </div>
     
 </>
